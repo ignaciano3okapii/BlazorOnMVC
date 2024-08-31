c87b5cdab3187273db507616d48622ce466d73d7
ignacio <igarciap@fi.uba.ar>
2024-08-31 02:10:22 -0400

    pongo codigo en archivo separado

---
+++ WebApplication1/Components/Nested.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Components;
+
+public partial class Nested
+{
+    private string message = string.Empty;
+    protected void SayHello()
+    {
+        message = "hello";
+    }
+}
+
